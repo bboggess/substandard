863f0e9965413c16c2868cd0321332364990d8a8
bboggess <b.boggess727@gmail.com>
2020-01-31 10:33:46 -0600

    Playlist fragment functioning. Still need ability to edit and show album covers in playlist.

--- app/src/main/java/com/example/substandard/ui/main/MediaPlayerLayout.java
+++ app/src/main/java/com/example/substandard/ui/main/MediaPlayerLayout.java
@@ -10,7 +10,6 @@ import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -50,7 +49,6 @@ public class MediaPlayerLayout extends LinearLayout {
 
     private MediaPlayerViewModel viewModel;
 
-    private FrameLayout fragmentContainer;
     // Save this so we can unregister on cleanup
     private MediaControllerCompat.Callback controllerCallback;
 
